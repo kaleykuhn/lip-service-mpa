1198a95a64b2434015b2e68850373775a1f27b9f
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-26 23:13:17 -0700

    changed back after trying nav bar option

--- src/components/pages/YourLooks.js
+++ src/components/pages/YourLooks.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../ui/Header";
+
 import Navigation from "../ui/Navigation";
 import AppTemplate from "../ui/AppTemplate";
 export default function YourLooks() {
