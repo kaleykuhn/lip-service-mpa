562b9a23eb7c40fcfa5d4a1dc40f110f80c408d5
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-28 00:41:41 -0700

    add structure to saved looks

--- src/components/pages/LipServiceQuiz.js
+++ src/components/pages/LipServiceQuiz.js
@@ -3,264 +3,80 @@ import Header from "../ui/Header";
 import Navigation from "../ui/Navigation";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
+import appLogo from "../../icons/rote-lippen.png";
 export default function LipServiceQuiz() {
    return (
       <AppTemplate>
          <Header />
          <Navigation />
+
+         <h2>Quiz</h2>
          <div className="mb-5"></div>
-
-         <p className="mb-4">Give your Lips the personality they deserve!</p>
-         <br></br>
-         <div>
-            <p className="mb-2">
-               Which of the following most describes yourself ?
-            </p>
-            <div className="form-check form-check-inline ">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio1"
-                  value="option1"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio1">
-                  Daring
-               </label>
+         <div className="row">
+            <div className="col">
+               <h3 className="mb-4 offset-1">
+                  Give your Lips the personality they deserve!
+               </h3>
             </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio2"
-                  value="option2"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio2">
-                  Reserved
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio3">
-                  Compassionate
-               </label>
-            </div>
-            <div className="mb-5"></div>
-            <p className="mb-2">
-               Which of the following most describes yourself ?
-            </p>
-            <div className="form-check form-check-inline ">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio1"
-                  value="option1"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio1">
-                  Daring
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio2"
-                  value="option2"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio2">
-                  Reserved
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio3">
-                  Compassionate
-               </label>
-            </div>
-            <div className="mb-5"></div>
-            <p className="mb-2">
-               Which of the following most describes yourself ?
-            </p>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio1"
-                  value="option1"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio1">
-                  Daring
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio2"
-                  value="option2"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio2">
-                  Reserved
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio3">
-                  Compassionate
-               </label>
-            </div>
-            <div className="mb-5"></div>
-            <p className="mb-2">
-               Which of the following most describes yourself ?
-            </p>
-            <div className="form-check form-check-inline ">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio1"
-                  value="option1"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio1">
-                  Daring
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio2"
-                  value="option2"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio2">
-                  Reserved
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio3">
-                  Compassionate
-               </label>
-            </div>
-            <div className="mb-5"></div>
-            <p className="mb-2">
-               Which of the following most describes yourself ?
-            </p>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio1"
-                  value="option1"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio1">
-                  Daring
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio2"
-                  value="option2"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio2">
-                  Reserved
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio3">
-                  Compassionate
-               </label>
-            </div>
-            <div className="mb-5"></div>
-            <p className="mb-2">
-               Which of the following most describes yourself ?
-            </p>
-            <div className="form-check form-check-inline ">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio1"
-                  value="option1"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio1">
-                  Daring
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio2"
-                  value="option2"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio2">
-                  Reserved
-               </label>
-            </div>
-            <div className="form-check form-check-inline">
-               <input
-                  className="form-check-input"
-                  type="radio"
-                  name="inlineRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-               />
-               <label className="form-check-label" htmlFor="inlineRadio3">
-                  Compassionate
-               </label>
-            </div>
-
-            <div className="mb-5"></div>
          </div>
+         <br></br>
+         <div className="card bg-secondary">
+            <img
+               src={appLogo}
+               width="46px;"
+               alt="Lip Service Lips"
+               className="mb-1 ml-2"
+            />
+            <div className="card-body bg-dark">
+               Which of the following most describes yourself ?
+               <div className="form-check  ">
+                  <input
+                     className="form-check-input"
+                     type="radio"
+                     name="inlineRadioOptions"
+                     id="inlineRadio1"
+                     value="option1"
+                  />
+                  <label className="form-check-label" htmlFor="inlineRadio1">
+                     Daring
+                  </label>
+               </div>
+               <div className="form-check ">
+                  <input
+                     className="form-check-input"
+                     type="radio"
+                     name="inlineRadioOptions"
+                     id="inlineRadio2"
+                     value="option2"
+                  />
+                  <label className="form-check-label" htmlFor="inlineRadio2">
+                     Reserved
+                  </label>
+               </div>
+               <div className="form-check form-check-inline">
+                  <input
+                     className="form-check-input"
+                     type="radio"
+                     name="inlineRadioOptions"
+                     id="inlineRadio3"
+                     value="option3"
+                  />
+                  <label className="form-check-label" htmlFor="inlineRadio3">
+                     Compassionate
+                  </label>
+               </div>
+               <div>
+                  <button className="btn btn-primary">save</button>
+               </div>
+            </div>
+         </div>
+         <div className="mb-5"></div>
          <Link
             to="/lip-results"
-            className="btn btn-outline-primary btn-lg float-right "
+            className="btn btn-outline-secondary btn-lg float-right "
             id="nextButton"
          >
-            Next
+            Submit
          </Link>
       </AppTemplate>
    );
--- src/components/pages/YourLooks.js
+++ src/components/pages/YourLooks.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../ui/Header";
-
+import appLogo from "../../img/comfortmattetemperud.jpg";
 import Navigation from "../ui/Navigation";
 import AppTemplate from "../ui/AppTemplate";
 export default function YourLooks() {
@@ -8,8 +8,124 @@ export default function YourLooks() {
       <AppTemplate>
          <Header />
          <Navigation />
+         <h2>Your Looks</h2>
          <div className="mb-5"></div>
          <p className="mb-4">Give your Lips the personality they deserve!</p>
+         <div className="mt-8">
+            <img
+               src={appLogo}
+               width="46px;"
+               alt="Lip Service Lips"
+               className="mb-1 "
+            />
+         </div>
+         <div className="mt-8">
+            <img
+               src={appLogo}
+               width="46px;"
+               alt="Lip Service Lips"
+               className="mb-1 "
+            />
+         </div>
+         <div className="mt-8">
+            <img
+               src={appLogo}
+               width="46px;"
+               alt="Lip Service Lips"
+               className="mb-1 "
+            />
+         </div>
+         <div className="card bg-light mb-3" style={{ maxWidth: "18rem" }} />
+         <div className="card-header">Match</div>
+         <div className="card-body">
+            <img
+               src={appLogo}
+               width="46px;"
+               alt="Lip Service Lips"
+               className="mb-1 ml-2"
+            />
+            <h4 id="cardfontcolor"> Name:Hitch Hike</h4>
+            <h4 id="cardfontcolor"> Finish: Comfort Matte</h4>
+            <h4 id="cardfontcolor"> Brand: Urban Decay</h4>
+            <a
+               href="https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html"
+               className="btn btn-primary float-right btn-sm"
+            >
+               Buy Now
+            </a>
+            <h5 className="card-title">Light card title</h5>
+            <p className="card-text" id="cardfontcolor">
+               {" "}
+               These are your personalized lip matches.The buy now buttons that
+               are provided above will instantly take you to purchase your
+               personalized lip color.
+            </p>
+            <div>
+               <button className="btn btn-danger">delete</button>
+            </div>
+         </div>
+         <div className="mb-8"></div>
+         <div className="card bg-light mb-3" style={{ maxWidth: "18rem" }} />
+         <div className="card-header">Match</div>
+         <div className="card-body">
+            <img
+               src={appLogo}
+               width="46px;"
+               alt="Lip Service Lips"
+               className="mb-1 ml-2"
+            />
+            <h4 id="cardfontcolor"> Name:Hitch Hike</h4>
+            <h4 id="cardfontcolor"> Finish: Comfort Matte</h4>
+            <h4 id="cardfontcolor"> Brand: Urban Decay</h4>
+            <a
+               href="https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html"
+               className="btn btn-primary float-right btn-sm"
+            >
+               Buy Now
+            </a>
+            <h5 className="card-title">Light card title</h5>
+            <p className="card-text" id="cardfontcolor">
+               {" "}
+               These are your personalized lip matches.The buy now buttons that
+               are provided above will instantly take you to purchase your
+               personalized lip color.
+            </p>
+            <div>
+               <button className="btn btn-danger">delete</button>
+            </div>
+         </div>
+         <div className="mb-8"></div>
+         <div className="mb-8"></div>
+         <div className="card bg-light mb-3" style={{ maxWidth: "18rem" }} />
+         <div className="card-header">Match</div>
+         <div className="card-body">
+            <img
+               src={appLogo}
+               width="46px;"
+               alt="Lip Service Lips"
+               className="mb-1 ml-2"
+            />
+            <h4 id="cardfontcolor"> Name:Hitch Hike</h4>
+            <h4 id="cardfontcolor"> Finish: Comfort Matte</h4>
+            <h4 id="cardfontcolor"> Brand: Urban Decay</h4>
+            <a
+               href="https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html"
+               className="btn btn-primary float-right btn-sm"
+            >
+               Buy Now
+            </a>
+            <h5 className="card-title">Light card title</h5>
+            <p className="card-text" id="cardfontcolor">
+               {" "}
+               These are your personalized lip matches.The buy now buttons that
+               are provided above will instantly take you to purchase your
+               personalized lip color.
+            </p>
+            <div>
+               <button className="btn btn-danger">delete</button>
+            </div>
+         </div>
+         <div className="mb-8"></div>
       </AppTemplate>
    );
 }
