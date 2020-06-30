a91db3fdf5987d5dc5b19fcc929f18fb75f5193a
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-29 17:57:53 -0700

    add const lipsticks data

---
+++ src/mock-data/lipsticks.js
@@ -0,0 +1,55 @@
+const lipsticks = [
+   {
+      id: "2adb6881-0e9e-4e3d-98ca-5da323791196",
+      name: "Hitch Hike",
+      brand: "Urban Decay",
+      color: "Brick Rose",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dwec22555b/ViceLipstickLPImages/ModelImages/3605971157073_vicelipstick_comfortmatte_hitchhike_alt4.jpg?sw=570&sh=570&sm=fit&q=70",
+      finish: "Comfort Matte",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html",
+
+      tags: [
+         {
+            id: "d996f5fe-342a-42ba-bee8-329e78b13307",
+            answer: "Reserved",
+         },
+      ],
+   },
+   {
+      id: "295c5b38-4253-4fed-b226-786c5ce8e5d0",
+      name: "Big Bang",
+      brand: "Urban Decay",
+      color: "Dark Fuschia",
+      finish: "Metalized",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dw732ed6a6/ViceLipstickLPImages/ModelImages/3605971155833_vicelipstick_metalized_bigbang_alt6.jpg?sw=570&sh=570&sm=fit&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/on/demandware.store/Sites-urbandecay-us-Site/default/Product-Variation?pid=ud771&dwvar_ud771_color=BIG%20BANG%20%28METALLIZED%29",
+      tags: [
+         {
+            id: "942d4afe-5370-400d-b71f-d9f61c644f3e",
+            answer: "Daring",
+         },
+      ],
+   },
+   {
+      id: "eebd3959-501e-45e1-834e-4ddbec267328",
+      name: "Love Light",
+      brand: "Urban Decay",
+      color: "Dark Coral",
+      finish: "Sheer Shimmer",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dw01e19522/ViceLipstickLPImages/ModelImages/3605971157394_vicelipstick_sheershimmer_lovelight_alt4.jpg?sw=570&sh=570&sm=fit&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/3605971157394.html?quantity=1&uuid=fba468fe1b4bcdf8449768795a",
+
+      tags: [
+         {
+            id: "bc137dc8-74e1-402c-b2e3-f83a5aa75c32",
+            answer: "Compassionate",
+         },
+      ],
+   },
+];
