3439a79dad32b22c6e1171c84f12e0390586eda6
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-29 17:57:26 -0700

    add lipstick schema

---
+++ src/mock-data/lipsticks-schema.js
@@ -0,0 +1,11 @@
+const lipSticksSchema = {
+   id: String,
+   name: String,
+   brand: String,
+   color: String,
+   desc: String,
+   finish: String,
+   buyNowUrl: String,
+   modelImageUrl: String,
+   tags: String,
+};
