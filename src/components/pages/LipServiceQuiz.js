4321db16e2a674c7a89a5d1a46065698a5c4a735
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-30 13:51:30 -0700

    add id from data into questions and change names

--- src/components/pages/LipServiceQuiz.js
+++ src/components/pages/LipServiceQuiz.js
@@ -37,7 +37,7 @@ export default function LipServiceQuiz() {
                      type="radio"
                      name="quizquestion1radio"
                      id="942d4afe-5370-400d-b71f-d9f61c644f3e"
-                     value="option1"
+                     value="942d4afe-5370-400d-b71f-d9f61c644f3e"
                   />
                   <label
                      className="custom-control-label"
@@ -52,7 +52,7 @@ export default function LipServiceQuiz() {
                      type="radio"
                      name="quizquestion1radio"
                      id="d996f5fe-342a-42ba-bee8-329e78b13307"
-                     value="option2"
+                     value="d996f5fe-342a-42ba-bee8-329e78b13307"
                   />
                   <label
                      className="custom-control-label"
@@ -67,8 +67,9 @@ export default function LipServiceQuiz() {
                      type="radio"
                      name="quizquestion1radio"
                      id="bc137dc8-74e1-402c-b2e3-f83a5aa75c32"
-                     value="option3"
+                     value="bc137dc8-74e1-402c-b2e3-f83a5aa75c32"
                   />
+
                   <label
                      className="custom-control-label"
                      htmlFor="bc137dc8-74e1-402c-b2e3-f83a5aa75c32"
@@ -89,28 +90,28 @@ export default function LipServiceQuiz() {
                className="mb-1 ml-2"
             />
             <div className="card-body bg-dark">
-               Which of the following most describes yourself ?
+               Which of these options best describes you?
                <div className="mb-3"></div>
                <div className="custom-control custom-radio">
                   <input
                      className="custom-control-input"
                      type="radio"
-                     name="inlineRadioOptions"
-                     id="inlineRadio1"
-                     value="option1"
+                     name="quizquestion2radio"
+                     id="89887a54-ce4c-40b3-968a-4a0e10c4f4ab"
+                     value="89887a54-ce4c-40b3-968a-4a0e10c4f4ab"
                   />
                   <label
                      className="custom-control-label"
-                     htmlFor="inlineRadio1"
+                     htmlFor="89887a54-ce4c-40b3-968a-4a0e10c4f4ab"
                   >
-                     Daring
+                     Communicative
                   </label>
                </div>
                <div className="custom-control custom-radio">
                   <input
-                     className="custom-control-input "
+                     className="custom-control-input  "
                      type="radio"
-                     name="inlineRadioOptions"
+                     name="quizquestion2radio"
                      id="inlineRadio2"
                      value="option2"
                   />
@@ -118,22 +119,22 @@ export default function LipServiceQuiz() {
                      className="custom-control-label"
                      htmlFor="inlineRadio2"
                   >
-                     Reserved
+                     Discreet
                   </label>
                </div>
                <div className="custom-control custom-radio">
                   <input
                      className="custom-control-input"
                      type="radio"
-                     name="inlineRadioOptions"
-                     id="inlineRadio3"
+                     name="quizquestion2radio"
+                     id="9a7444b0-8b2f-40c6-91fd-0b57107f1d97"
                      value="option3"
                   />
                   <label
                      className="custom-control-label"
-                     htmlFor="inlineRadio3"
+                     htmlFor="9a7444b0-8b2f-40c6-91fd-0b57107f1d97"
                   >
-                     Compassionate
+                     Candid
                   </label>
                </div>
                <div className="mb-4"></div>
@@ -148,51 +149,55 @@ export default function LipServiceQuiz() {
                className="mb-1 ml-2"
             />
             <div className="card-body bg-dark">
-               Which of the following most describes yourself ?
+               I value which one of these qualities the most?
                <div className="mb-3"></div>
                <div className="custom-control custom-radio">
                   <input
                      className="custom-control-input"
                      type="radio"
-                     name="inlineRadioOptions"
-                     id="inlineRadio1"
-                     value="option1"
+                     name="quizquestion3radio"
+                     id="57a75a90-1d93-427a-9913-9254843d373b"
+                     value="57a75a90-1d93-427a-9913-9254843d373b"
                   />
+
                   <label
                      className="custom-control-label"
-                     htmlFor="inlineRadio1"
+                     htmlFor="57a75a90-1d93-427a-9913-9254843d373b"
                   >
-                     Daring
+                     Creativity
                   </label>
                </div>
                <div className="custom-control custom-radio">
                   <input
                      className="custom-control-input "
                      type="radio"
-                     name="inlineRadioOptions"
-                     id="inlineRadio2"
-                     value="option2"
+                     name="quizquestion3radio"
+                     id="a74c46e3-54d4-4694-a725-5b15e5e06689"
+                     value="a74c46e3-54d4-4694-a725-5b15e5e06689"
                   />
+
                   <label
                      className="custom-control-label"
-                     htmlFor="inlineRadio2"
+                     htmlFor="a74c46e3-54d4-4694-a725-5b15e5e06689"
                   >
-                     Reserved
+                     {" "}
+                     Decisiveness
                   </label>
                </div>
                <div className="custom-control custom-radio">
                   <input
                      className="custom-control-input"
                      type="radio"
-                     name="inlineRadioOptions"
-                     id="inlineRadio3"
-                     value="option3"
+                     name="quizquestion3radio"
+                     id="8bbe9a09-0d6d-45be-852d-b3346332bedc"
+                     value="8bbe9a09-0d6d-45be-852d-b3346332bedc"
                   />
+
                   <label
                      className="custom-control-label"
-                     htmlFor="inlineRadio3"
+                     htmlFor="8bbe9a09-0d6d-45be-852d-b3346332bedc"
                   >
-                     Compassionate
+                     Sincerity
                   </label>
                </div>
                <div className="mb-4"></div>
@@ -207,51 +212,54 @@ export default function LipServiceQuiz() {
                className="mb-1 ml-2"
             />
             <div className="card-body bg-dark">
-               Which of the following most describes yourself ?
+               Which of these qualities do I exhibit most?
                <div className="mb-3"></div>
                <div className="custom-control custom-radio">
                   <input
                      className="custom-control-input"
                      type="radio"
-                     name="inlineRadioOptions"
-                     id="inlineRadio1"
-                     value="option1"
+                     name="quizquestion4radio"
+                     id="ac1ab5d3-1c67-4afc-94c1-c2765efcaa9f"
+                     value="ac1ab5d3-1c67-4afc-94c1-c2765efcaa9f"
                   />
+
                   <label
                      className="custom-control-label"
-                     htmlFor="inlineRadio1"
+                     htmlFor="ac1ab5d3-1c67-4afc-94c1-c2765efcaa9f"
                   >
-                     Daring
+                     {" "}
+                     Dominance
                   </label>
                </div>
                <div className="custom-control custom-radio">
                   <input
                      className="custom-control-input "
                      type="radio"
-                     name="inlineRadioOptions"
-                     id="inlineRadio2"
-                     value="option2"
+                     name="quizquestion4radio"
+                     id="5ae236f3-c9e4-4fc3-9f55-5322896c6c60"
+                     value="5ae236f3-c9e4-4fc3-9f55-5322896c6c60"
                   />
                   <label
                      className="custom-control-label"
-                     htmlFor="inlineRadio2"
+                     htmlFor="5ae236f3-c9e4-4fc3-9f55-5322896c6c60"
                   >
-                     Reserved
+                     Loyalty
                   </label>
                </div>
                <div className="custom-control custom-radio">
                   <input
                      className="custom-control-input"
                      type="radio"
-                     name="inlineRadioOptions"
-                     id="inlineRadio3"
-                     value="option3"
+                     name="quizquestion4radio"
+                     id="08cc0730-3cd1-490c-a7f3-d3f44e14dede"
+                     value="08cc0730-3cd1-490c-a7f3-d3f44e14dede"
                   />
+
                   <label
                      className="custom-control-label"
-                     htmlFor="inlineRadio3"
+                     htmlFor="08cc0730-3cd1-490c-a7f3-d3f44e14dede"
                   >
-                     Compassionate
+                     Open Mindedness
                   </label>
                </div>
                <div className="mb-4"></div>
@@ -266,7 +274,7 @@ export default function LipServiceQuiz() {
                className="mb-1 ml-2"
             />
             <div className="card-body bg-dark">
-               Which of the following most describes yourself ?
+               Which of the following describes your lifestyle?
                <div className="mb-3"></div>
                <div className="custom-control custom-radio">
                   <input
@@ -280,7 +288,7 @@ export default function LipServiceQuiz() {
                      className="custom-control-label"
                      htmlFor="inlineRadio1"
                   >
-                     Daring
+                     Active
                   </label>
                </div>
                <div className="custom-control custom-radio">
@@ -295,7 +303,7 @@ export default function LipServiceQuiz() {
                      className="custom-control-label"
                      htmlFor="inlineRadio2"
                   >
-                     Reserved
+                     Modern
                   </label>
                </div>
                <div className="custom-control custom-radio">
@@ -310,7 +318,7 @@ export default function LipServiceQuiz() {
                      className="custom-control-label"
                      htmlFor="inlineRadio3"
                   >
-                     Compassionate
+                     Laid Back
                   </label>
                </div>
                <div className="mb-4"></div>
@@ -325,7 +333,7 @@ export default function LipServiceQuiz() {
                className="mb-1 ml-2"
             />
             <div className="card-body bg-dark">
-               Which of the following most describes yourself ?
+               Which of these most describes your astethic?
                <div className="mb-3"></div>
                <div className="custom-control custom-radio">
                   <input
@@ -339,7 +347,7 @@ export default function LipServiceQuiz() {
                      className="custom-control-label"
                      htmlFor="inlineRadio1"
                   >
-                     Daring
+                     Classy
                   </label>
                </div>
                <div className="custom-control custom-radio">
@@ -354,7 +362,7 @@ export default function LipServiceQuiz() {
                      className="custom-control-label"
                      htmlFor="inlineRadio2"
                   >
-                     Reserved
+                     Edgy
                   </label>
                </div>
                <div className="custom-control custom-radio">
@@ -369,7 +377,7 @@ export default function LipServiceQuiz() {
                      className="custom-control-label"
                      htmlFor="inlineRadio3"
                   >
-                     Compassionate
+                     Casual
                   </label>
                </div>
                <div className="mb-4"></div>
