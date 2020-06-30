d78c4c2ce79efa59cb3a81bbbec10f91143fb94e
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-30 13:52:00 -0700

    add mock data

--- src/mock-data/users.js
+++ src/mock-data/users.js
@@ -9,6 +9,18 @@ const users = [
             id: "d996f5fe-342a-42ba-bee8-329e78b13307",
             answer: "Reserved",
          },
+         {
+            id: "194b8bb1-c609-4686-8772-8d84a850dc57",
+            answer: "Discreet",
+         },
+         {
+            id: "57a75a90-1d93-427a-9913-9254843d373b",
+            answer: "Creativity",
+         },
+         {
+            id: "08cc0730-3cd1-490c-a7f3-d3f44e14dede",
+            answer: "Open Mindedness",
+         },
       ],
    },
    {
@@ -20,6 +32,19 @@ const users = [
          {
             id: "942d4afe-5370-400d-b71f-d9f61c644f3e",
             answer: "Daring",
+         },
+
+         {
+            id: "9a7444b0-8b2f-40c6-91fd-0b57107f1d97",
+            answer: "Candid",
+         },
+         {
+            id: "8bbe9a09-0d6d-45be-852d-b3346332bedc",
+            answer: "Sincerity ",
+         },
+         {
+            id: "5ae236f3-c9e4-4fc3-9f55-5322896c6c60",
+            answer: "Loyalty",
          },
       ],
    },
