a83d84ac7affef4feebc2326a3dc5524c6c119a1
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-30 13:51:43 -0700

    add moch data

--- src/mock-data/lipsticks.js
+++ src/mock-data/lipsticks.js
@@ -52,4 +52,185 @@ const lipsticks = [
          },
       ],
    },
+   {
+      id: "55e1d1fa-000d-4dbb-9664-7b70077ea3d9",
+      name: "714",
+      brand: "Urban Decay",
+      color: "Bright Red",
+      finish: "Mega Matte",
+      desc:
+         "Urban Decay 714 is a neutral-toned, medium-dark red with a matte finish",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dwc9a48aa8/ViceLipstickLPImages/ModelImages/3605971155390_vicelipstick_megamatte_714_alt5.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=714%20%28MEGA%20MATTE%29",
+
+      tags: [
+         {
+            id: "9a7444b0-8b2f-40c6-91fd-0b57107f1d97",
+            answer: "Candid",
+         },
+      ],
+   },
+   {
+      id: "205027d3-8cda-44ac-83d2-3de5e7ac9fb6",
+      name: "Jilted",
+      brand: "Urban Decay",
+      color: "Deep Fuschia",
+      finish: "Cream",
+      desc:
+         "Is described as a deep fuchsia shimmer with blue shift with a Cream finish. It's a bright, medium-dark fuchsia-berry with subtle, cool undertones and fuchsia shimmer.",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dwb1fa7322/ViceLipstickLPImages/ModelImages/3605971157233_vicelipstick_cream_jilted_alt7.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=JILTED%20%28CREAM%29",
+
+      tags: [
+         {
+            id: "89887a54-ce4c-40b3-968a-4a0e10c4f4ab",
+            answer: "Communicative",
+         },
+      ],
+   },
+   {
+      id: "98d0ef9f-f1ec-4139-8ad6-4f50f55308ab",
+      name: "Oblivian",
+      brand: "Urban Decay",
+      color: "Gray Taupe",
+      finish: "Mega Matte",
+      desc:
+         "Urban Decay Oblivion is a very cool-toned, medium taupe with a matte finish",
+
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dwd1c28f17/ProductImages/Lips/ViceLipstick/3605971157837_vicelipstick_megamatte_oblivion_alt6.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=Oblivion%20%28MEGA%20MATTE%29",
+
+      tags: [
+         {
+            id: "194b8bb1-c609-4686-8772-8d84a850dc57",
+            answer: "Discreet",
+         },
+      ],
+   },
+   {
+      id: "1451a575-340d-4004-866b-346396c989cc",
+      name: "Crash",
+      brand: "Urban Decay",
+      color: "Neon Orange",
+      finish: "Mega Matte",
+      desc:
+         "Urban Decay Crash is a very warm-toned, medium orange with a matte finish",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dw9aa276d3/ProductImages/Lips/ViceLipstick/3605971156236_vicelipstick_megamatte_crash_alt4.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=Crash%20%28MEGA%20MATTE%29",
+
+      tags: [
+         {
+            id: "57a75a90-1d93-427a-9913-9254843d373b",
+            answer: "Creativity",
+         },
+      ],
+   },
+   {
+      id: "aa6554cc-668d-4f73-b927-876c28480ca3",
+      name: "Mrs. Mia Wallace",
+      brand: "Urban Decay",
+      color: "Deep Blood Red",
+      finish: "Cream",
+      desc:
+         "Urban Decay Mrs. Mia Wallace is a moderately warm-toned, dark red with a cream finish",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dwdbb7e44f/ViceLipstickLPImages/ModelImages/3605971157592_vicelipstick_cream_mrsmiawallace_alt6.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=MRS.%20MIA%20WALLACE%20%28CREAM%29",
+
+      tags: [
+         {
+            id: "a74c46e3-54d4-4694-a725-5b15e5e06689",
+            answer: " Decisiveness",
+         },
+      ],
+   },
+   {
+      id: "7355c440-ac02-45b2-aa4a-52192248965d",
+      name: "Violate",
+      brand: "Urban Decay",
+      color: "Dusty Purple",
+      finish: "Cream",
+      desc:
+         "Urban Decay Violate is a slightly warm-toned, light-medium mauve with a cream finish",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dw25a68a6c/ViceLipstickLPImages/ModelImages/3605971159718_vicelipstick_cream_violate_alt7.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=VIOLATE%20%28CREAM%29",
+
+      tags: [
+         {
+            id: "8bbe9a09-0d6d-45be-852d-b3346332bedc",
+            answer: " Sincerity ",
+         },
+      ],
+   },
+   {
+      id: "745c38a7-f05c-423b-a5bb-71d4587ec605",
+      name: "Shame",
+      brand: "Urban Decay",
+      color: "Deep Berry",
+      finish: "Cream",
+      desc:
+         "Urban Decay Shame is a slightly warm-toned, dark burgundy with a pearl finish",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dweac55e90/ViceLipstickLPImages/ModelImages/3605971158599_vicelipstick_cream_shame_alt4.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=SHAME%20%28CREAM%29",
+
+      tags: [
+         {
+            id: "ac1ab5d3-1c67-4afc-94c1-c2765efcaa9f",
+            answer: "Dominance",
+         },
+      ],
+   },
+   {
+      id: "00de5359-fa23-4e5d-9223-31221cdfd439",
+      name: "Heroine",
+      brand: "Urban Decay",
+      color: "Navy Blue",
+      finish: "Comfort Matte",
+      desc:
+         "Urban Decay Heroine is a very cool-toned, dark blue with a satin finish",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dw123d506d/ViceLipstickLPImages/ModelImages/3605971156991_vicelipstick_comfortmatte_heroine_alt7.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=HEROINE%20%28COMFORT%20MATTE%29",
+
+      tags: [
+         {
+            id: "5ae236f3-c9e4-4fc3-9f55-5322896c6c60",
+            answer: "Loyalty",
+         },
+      ],
+   },
+   {
+      id: "81dece61-4e19-4e7d-9b83-0a8527ef0ad7",
+      name: "Pandemonium",
+      brand: "Urban Decay",
+      color: "Bright Purple",
+      finish: "Mega Matte",
+      desc:
+         "Urban Decay Pandemonium is a slightly warm-toned, dark purple with a satin finish",
+      modelImageUrl:
+         "https://www.urbandecay.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dwa53fc9f9/ViceLipstickLPImages/ModelImages/3605971157912_vicelipstick_megamatte_pandemonium_alt5.jpg?sw=465&sfrm=jpg&q=70",
+      buyNowUrl:
+         "https://www.urbandecay.com/vice-lipstick-by-urban-decay/ud771.html?dwvar_ud771_color=PANDEMONIUM%20%28MEGA%20MATTE%29",
+
+      tags: [
+         {
+            id: "08cc0730-3cd1-490c-a7f3-d3f44e14dede",
+            answer: "Open Mindedness",
+         },
+      ],
+   },
 ];
