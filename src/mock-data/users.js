67eff7b2bf5b52d1376e15686960f0dfd65c903a
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-07-01 11:18:12 -0700

    change createdAt number format

--- src/mock-data/users.js
+++ src/mock-data/users.js
@@ -3,7 +3,7 @@ const users = [
       id: "d9d40e8b-d863-4718-87ee-264df43e7057",
       email: "kaleyK@gmail.com",
       password: "A267F7DBA707256B0B664DEE86AB9AE8B4941218",
-      createdAt: "1593454553 ",
+      createdAt: "1593627051313 ",
       tags: [
          {
             id: "d996f5fe-342a-42ba-bee8-329e78b13307",
@@ -27,7 +27,7 @@ const users = [
       id: "6aee7261-0352-41f5-b410-072aafb34b56",
       email: "darielleO@gmail.com",
       password: "5944AE25418CEABCF285DCA1D721B77888DAC89B",
-      createdAt: "1593454606",
+      createdAt: "1593627085106",
       tags: [
          {
             id: "942d4afe-5370-400d-b71f-d9f61c644f3e",
@@ -52,7 +52,7 @@ const users = [
       id: "6c091285-644d-455a-800f-921656c2b2de",
       email: "KateC@gmail.com",
       password: "16C92AE31774F26F2314CC8061D4768891D19381",
-      createdAt: "1593568980",
+      createdAt: "1593627121568",
       tags: [
          {
             id: "bc137dc8-74e1-402c-b2e3-f83a5aa75c32",
